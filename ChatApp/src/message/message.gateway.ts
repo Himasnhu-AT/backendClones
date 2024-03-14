33f2007e5e2a3fbfe6c122638d12929e1a784419
Himanshu-AT <aristech.official@gmail.com>
2024-03-14 22:25:07 +0530

    add relevant comments

--- ChatApp/src/message/message.gateway.ts
+++ ChatApp/src/message/message.gateway.ts
@@ -21,6 +21,12 @@ export class MessageGateway {
   constructor(private readonly messageService: MessageService) {}
 
   @SubscribeMessage('createMessage')
+  /**
+   * Creates a new message.
+   *
+   * @param createMessageDto - The data for creating the message.
+   * @returns The created message.
+   */
   async create(@MessageBody() createMessageDto: CreateMessageDto) {
     const message = await this.messageService.create(createMessageDto);
     this.server.emit('message', message);
@@ -28,11 +34,22 @@ export class MessageGateway {
   }
 
   @SubscribeMessage('findAllMessage')
+  /**
+   * Retrieves all messages.
+   * @returns {Promise<Message[]>} A promise that resolves to an array of messages.
+   */
   findAll() {
     return this.messageService.findAll();
   }
 
   @SubscribeMessage('join')
+  /**
+   * Joins a room with the specified name.
+   *
+   * @param name - The name of the room to join.
+   * @param client - The connected socket client.
+   * @returns A promise that resolves to the identification result.
+   */
   joinRoom(
     @MessageBody('name') name: string,
     @ConnectedSocket() client: Socket,
@@ -41,6 +58,12 @@ export class MessageGateway {
   }
 
   @SubscribeMessage('typing')
+  /**
+   * Handles the typing event.
+   *
+   * @param isTyping - A boolean indicating whether the user is typing or not.
+   * @param client - The connected socket client.
+   */
   typing(
     @MessageBody('isTyping') isTypeing: boolean,
     @ConnectedSocket() client: Socket,
